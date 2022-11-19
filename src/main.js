d6e911d0f4771c0cb5fdfaba9a13dce4cbad31a4
intmain84 <aheleha@gmail.com>
2022-11-19 11:58:22 +0600

    fff

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from "./App.vue";
 import TeamsList from "./components/teams/TeamsList.vue";
 import UsersList from "./components/users/UsersList.vue";
 import TeamMembers from "./components/teams/TeamMembers.vue";
-//import NotFound from "./components/UI/NotFound.vue";
+import NotFound from "./components/UI/NotFound.vue";
 
 const app = createApp(App);
 
@@ -25,7 +25,7 @@ const router = createRouter({
     },
     { path: "/users", component: UsersList },
     { path: "/teams/:teamId", component: TeamMembers, props: true },
-    //{ path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
+    { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
   ],
   linkActiveClass: "active",
 });
